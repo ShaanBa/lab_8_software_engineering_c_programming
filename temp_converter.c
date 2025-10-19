4f15886b939a36306b0abcaec6319b21795ba655
Shaan Bawa <shaanbawa6@gmail.com>
2025-10-19 17:37:05 -0500

    Finished temp converter

--- temp_converter.c
+++ temp_converter.c
@@ -1,18 +1,86 @@
 #include <stdio.h>
+#include <ctype.h>
+
+double toCelsius(double temp, char scale);
+double fromCelsius(double tempC, char targetScale);
+void categorizeTemperature(double tempC);
 
 int main() {
-    double temperature;
-    // here ask for the input from the user for temp value
-    printf("Enter the temperature value: ");
-    scanf("%lf", &temperature);
-        
-    char original_scale;
-    printf("Enter the original scale (C, F, or K): ");
-    scanf(" %c", &original_scale);
+    double tempInput, convertedTemp, tempC;
+    char originalScale, targetScale;
 
-    char target_scale;
+    printf("Enter the temperature value: ");
+    scanf("%lf", &tempInput);
+
+    printf("Enter the original scale (C, F, or K): ");
+    scanf(" %c", &originalScale);
+    originalScale = toupper(originalScale);
+
     printf("Enter the scale to convert to (C, F, or K): ");
-    scanf("%c", &target_scale);
+    scanf(" %c", &targetScale);
+    targetScale = toupper(targetScale);
+
+    tempC = toCelsius(tempInput, originalScale);
+    if (tempC == -9999) {
+        printf("Invalid original scale entered.\n");
+        return 1;
+    }
+
+    convertedTemp = fromCelsius(tempC, targetScale);
+    if (convertedTemp == -9999) {
+        printf("Invalid target scale entered.\n");
+        return 1;
+    }
+
+    printf("Converted temperature: %.2lf %c\n", convertedTemp, targetScale);
+
+    categorizeTemperature(tempC);
 
     return 0;
+}
+
+double toCelsius(double temp, char scale) {
+    switch (scale) {
+        case 'C':
+            return temp;
+        case 'F':
+            return (temp - 32) * 5.0 / 9.0;
+        case 'K':
+            return temp - 273.15;
+        default:
+            return -9999;
+    }
+}
+
+double fromCelsius(double tempC, char targetScale) {
+    switch (targetScale) {
+        case 'C':
+            return tempC;
+        case 'F':
+            return (tempC * 9.0 / 5.0) + 32;
+        case 'K':
+            return tempC + 273.15;
+        default:
+            return -9999;
+    }
+}
+
+void categorizeTemperature(double tempC) {
+    printf("Temperature category: ");
+    if (tempC < 0) {
+        printf("Freezing\n");
+        printf("Weather advisory: Stay warm and avoid icy roads!\n");
+    } else if (tempC < 10) {
+        printf("Cold\n");
+        printf("Weather advisory: Wear a jacket.\n");
+    } else if (tempC < 25) {
+        printf("Comfortable\n");
+        printf("Weather advisory: Enjoy the nice weather.\n");
+    } else if (tempC < 35) {
+        printf("Hot\n");
+        printf("Weather advisory: Drink lots of water!\n");
+    } else {
+        printf("Extreme Heat\n");
+        printf("Weather advisory: Stay indoors and stay hydrated!\n");
+    }
 }
